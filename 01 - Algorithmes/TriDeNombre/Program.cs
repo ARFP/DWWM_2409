76117eebff0f5fe7d409421df8f7d2579e0a185e
MDevoldere <mdevoldere@users.noreply.github.com>
2024-09-24 10:13:32 +0200

    Fin TriDeNombre

--- 01 - Algorithmes/TriDeNombre/Program.cs
+++ 01 - Algorithmes/TriDeNombre/Program.cs
@@ -1,3 +1,37 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Tri de Nombres ++");
 
+// VARIABLES
+
+int numA;
+int numB;
+int numC;
+int[] nombres;
+string saisieUtilisateur;
+
+// TRAITEMENT
+
+Console.WriteLine("Entrez le premier nombre");
+saisieUtilisateur = Console.ReadLine(); // Récupération d'une saisie utilisateur
+numA = int.Parse(saisieUtilisateur); // Conversion de la saisie en nombre entier
+
+Console.WriteLine("Entrez le deuxième nombre");
+saisieUtilisateur = Console.ReadLine();
+numB = int.Parse(saisieUtilisateur);
+
+Console.WriteLine("Entrez le troisième nombre");
+saisieUtilisateur = Console.ReadLine();
+numC = int.Parse(saisieUtilisateur);
+
+nombres = [numA, numB, numC];
+
+Array.Sort(nombres);
+
+// AFFICHAGE
+
+    for (int i = 0; i < nombres.Length; i++)
+    {
+        Console.WriteLine(nombres[i]);
+    }
+    
+Console.ReadLine();
