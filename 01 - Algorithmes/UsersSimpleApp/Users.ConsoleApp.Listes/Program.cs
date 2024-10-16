e0964d1d53247891407cd3f47219d18faf5e9f92
MDevoldere <mdevoldere@users.noreply.github.com>
2024-10-16 10:26:08 +0200

    modif utilisateurs

--- 01 - Algorithmes/UsersSimpleApp/Users.ConsoleApp.Listes/Program.cs
+++ 01 - Algorithmes/UsersSimpleApp/Users.ConsoleApp.Listes/Program.cs
@@ -7,14 +7,12 @@
             Console.WriteLine("Enregistrement d'utilisateurs (gestion avec Listes).");
 
             //Variable
-            bool majeur;
             DateTime formaDate;
             DateTime ajd = DateTime.Now;           
             int age = 0;
             string saisieNomPrenom;
             string saisieDate;
             string? metierCouleur = null;
-            bool dateValide;
             ConsoleKey continuerO_N;
             List<string[]> utilisateurs = new();
 
@@ -26,6 +24,7 @@
 
                 Console.WriteLine("Saisissez la date de naissane, jour/mois/année.");
                 saisieDate = Console.ReadLine();
+
                 if (DateTime.TryParse(saisieDate, out formaDate))
                 {
                     TimeSpan intervalle = ajd - formaDate;
@@ -36,10 +35,9 @@
                 {
                     Console.WriteLine("Saisie non valide.");
                 }
-
                 else if (age<18)
                 {
-                    Console.WriteLine("couleur préféré ?");
+                    Console.WriteLine("couleur préférée ?");
                     metierCouleur = Console.ReadLine();
                 }
                 else
@@ -48,15 +46,33 @@
                     metierCouleur = Console.ReadLine();
                 }
 
-                string[] unUtilisateur = [saisieNomPrenom, saisieDate + " (" + age + ")", metierCouleur];
+                string[] unUtilisateur = [saisieNomPrenom, saisieDate, age.ToString(), metierCouleur];
+
+                utilisateurs.Add(unUtilisateur);
 
                 Console.WriteLine("Voulez vous saisir un autre utilisateur : N/O ?");
                 continuerO_N = Console.ReadKey(true).Key;
-
-
-
             }
             while (continuerO_N == ConsoleKey.O);
+
+            // Affichage de la liste des utilisateurs
+
+            // List<string[]> utilisateurs = new();
+            foreach (string[] utilisateur in utilisateurs)
+            {
+                Console.Write("Nom et Prénom: " + utilisateur[0] + " - ");
+                Console.Write("Date de naissance (âge) : " + utilisateur[1] + "(" + utilisateur[2] + ")");
+
+                if (int.Parse(utilisateur[2]) < 18)
+                {
+                    Console.WriteLine("Couleur: " + utilisateur[3]);
+                }
+                else
+                {
+                    Console.WriteLine("Métier: " + utilisateur[3]);
+                }
+                
+            }
 
 
         }
---
+++ 01 - Algorithmes/UsersSimpleApp/Users.ConsoleApp.Objects/Utilisateur.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Users.ConsoleApp.Objects
+{
+    public class Utilisateur
+    {
+        //private Guid id;
+        
+        private string nom;
+
+        private string prenom;
+
+        private DateTime dateDeNaissance;
+
+        private string? metier;
+
+        private string? couleurPreferee;
+
+
+        public Utilisateur()
+        {
+
+        }
+
+
+        public int GetAge()
+        {
+            return 0;
+        }
+
+        public bool IsMajeur()
+        {
+            return false;
+        }
+
+        public string GetCouleurOuMetier()
+        {
+            return "";
+        }
+
+        public string GetDateDeNaissance()
+        {
+            return "";
+        }
+
+        public string GetNomComplet()
+        {
+            return prenom + " " + nom;
+        }
+    }
+}
