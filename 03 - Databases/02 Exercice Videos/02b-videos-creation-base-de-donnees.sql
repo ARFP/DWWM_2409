011bfcc5c9493f513a430182534d2712d6ff797c
MDevoldere <10086604+mdevoldere@users.noreply.github.com>
2024-12-18 10:15:09 +0100

    organisationTP videos

---
+++ 03 - Databases/02 Exercice Videos/02b-videos-creation-base-de-donnees.sql
@@ -0,0 +1,23 @@
+/* commentaire */
+-- commentaire
+
+/* SUPPRIMER LA BASE DE DONNEES SI ELLE EXISTE */
+
+
+/* CREER UNE BASE DE DONNEES NOMMEE "videos" */
+
+
+/* UTILISER LA BASE DE DONNEES CREEE */
+
+
+-- Les requêtes qui suivent utiliseront 
+-- la base de données sélectionné ci-dessus
+
+
+/* CREER UNE TABLE NOMMEE "film" */
+
+
+
+
+
+/* CONTRAINTES */
---
+++ 03 - Databases/02 Exercice Videos/02c-videos-jeu-d-essai.sql
@@ -0,0 +1,18 @@
+/* INSERER LE JEU D'ESSAI DANS LA TABLE FILMS */
+INSERT INTO film 
+VALUES 
+(NULL, "Léon", 120),
+(NULL, "E.T", 90),
+(NULL, "ça", 103);
+
+INSERT INTO film 
+(film_titre, film_duree)
+VALUES 
+("L'exorciste", 240),
+("Super Papa", 87),
+("Gladiator 2", 117);
+
+INSERT INTO film 
+(film_duree, film_titre)
+VALUES 
+(98, "Loups-Garous");
